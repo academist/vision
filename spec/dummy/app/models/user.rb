5da676284bfff563ed44289e54d71c4858f7403b
shuji tateishi <stsmania17@gmail.com>
2023-10-31 20:06:54 +0900

    不要なカラムを削除

--- spec/dummy/app/models/user.rb
+++ spec/dummy/app/models/user.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  enum user_type: { supporter: 0, challenger: 1, researcher: 2 }
   has_one :vision_profile, class_name: 'Vision::Profile', dependent: :destroy
 end
--- spec/dummy/spec/factories/user.rb
+++ spec/dummy/spec/factories/user.rb
@@ -2,7 +2,5 @@ FactoryBot.define do
   factory :user do
     name { "John Doe" }
     email { "john@example.com" }
-    gender { 1 }
-    user_type { 1 }
   end
 end
