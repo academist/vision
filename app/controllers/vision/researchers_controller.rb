0feb69b90ac2120935bcd15afbe268da775deb18
shuji tateishi <stsmania17@gmail.com>
2023-11-17 01:42:45 +0900

    controllerおよびviewを追加

--- app/controllers/vision/researchers_controller.rb
+++ app/controllers/vision/researchers_controller.rb
@@ -3,5 +3,22 @@
 module Vision
   class ResearchersController < ApplicationController
     def index; end
+
+    def show
+      redirect_to researchers_path unless can_profile_browsing?
+    end
+
+    private
+
+    def can_profile_browsing?
+      return false if profile.blank?
+      return false unless profile.published?
+
+      true
+    end
+
+    def profile
+      @profile ||= ::Vision::Profile.find_by(user_id: params[:user_id])
+    end
   end
 end
