7c102c232e7b59cf118487090cce3b0ba613c8f1
shuji tateishi <stsmania17@gmail.com>
2023-12-05 03:26:26 +0900

    コメント削除

--- spec/dummy/spec/factories/vision/research_field.rb
+++ spec/dummy/spec/factories/vision/research_field.rb
@@ -3,6 +3,6 @@
 FactoryBot.define do
   factory :research_field, class: 'Vision::ResearchField' do
     profile factory: %i[vision_profile]
-    name { Faker::Lorem.word }  # 研究分野の名前をランダムに生成
+    name { Faker::Lorem.word }
   end
 end
